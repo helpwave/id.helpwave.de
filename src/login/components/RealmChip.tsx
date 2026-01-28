17d2e922a652d2de902bccb9395fb1aa8109f1b9
Felix Evers <git@felixevers.de>
2026-01-29 00:20:25 +0100

    hide realm on main

--- src/login/components/RealmChip.tsx
+++ src/login/components/RealmChip.tsx
@@ -8,11 +8,11 @@ type RealmChipProps = {
 
 export function RealmChip({ kcContext }: RealmChipProps) {
     const realmName = (kcContext.realm as { name?: string, realm?: string })?.realm ??
-                      (kcContext.realm as { name?: string })?.name ??
-                      'unknown'
+        (kcContext.realm as { name?: string })?.name ??
+        'unknown'
     const color = getRealmColor(realmName)
 
-    return (
+    return realmName === 'main' ? <></> : (
         <Chip color={color} size="md" aria-label={`realm: ${realmName}`}>
             {realmName}
         </Chip>
